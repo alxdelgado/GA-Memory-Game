80efea5ac5cad667cfb988db7220555e4a6ce89a
Alex Delgado <Alex@Alexs-MacBook-Pro.local>
2018-06-09 17:08:31 -0500

    Unit 9

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
 
-cardsInPlay.push('cardOne');
+var checkForMatch = function () {
 
-console.log("User flipped queen");
-
-var cardTwo = cards[3];
-
-cardsInPlay.push('cardTwo');
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		
+		alert("You found a match!");
+	} 
+	
 	else {
-		alert("Sorry try again");
+		alert("Sorry try again.");
 	}
+};
+
+if (cardsInPlay.length === 2) {
+}
+
+var flipCard = function (cardId) {
+	
+	cardsInPlay.push(cards[cardId]);
+	
+	console.log("User flipped" + cards[cardId]);
+
+	checkForMatch();
+};
+
+
+flipCard(0);
+flipCard(2);
